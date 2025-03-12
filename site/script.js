a02b4ddc46a6b4e4ed0a20979a2e7c46f75fe3af
Jannis Lauer <jannis280@outlook.de>
2025-03-12 17:23:37 +0100

    flexbox update

--- site/script.js
+++ site/script.js
@@ -1,7 +1,5 @@
 style = document.getElementById("style")
 black = true;
-lastid = -1;
-lastobj = "";
 
 function switchdarklightmode() {
     if (black) {
@@ -11,33 +9,5 @@ function switchdarklightmode() {
     else {
         style.href = "styleblack.css";
         black = true;
-    }
-}
-
-function getbigger(obj, id) {
-    if (lastid == id || lastobj == obj) {
-        for (let i = id; i < id + 1; i++) {
-            const element = b[i];
-            element.style.width = "360px";
-            element.style.height = "360px";
-        }
-        lastid = -1;
-        lastobj = "obj";
-    }
-    else if (obj = "b") {
-        b = document.getElementsByClassName("bbox")
-        for (let i = 0; i < b.length; i++) {
-            const element = b[i];
-            if (i == id) {
-                element.style.width = "auto";
-                element.style.height = "auto";
-            }
-            else {
-                element.style.width = "360px";
-                element.style.height = "360px";
-            }
-        }
-        lastid = id;
-        lastobj = obj;
     }
 }
