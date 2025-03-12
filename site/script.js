e4cde78361b1b50a60b5add8e53c48c05fd447f2
Jannis Lauer <jannis280@outlook.de>
2025-03-12 16:31:25 +0100

    updatet class and box verhalten und aufklapp mechanismen

--- site/script.js
+++ site/script.js
@@ -1,5 +1,7 @@
 style = document.getElementById("style")
 black = true;
+lastid = -1;
+lastobj = "";
 
 function switchdarklightmode() {
     if (black) {
@@ -9,5 +11,33 @@ function switchdarklightmode() {
     else {
         style.href = "styleblack.css";
         black = true;
+    }
+}
+
+function getbigger(obj, id) {
+    if (lastid == id || lastobj == obj) {
+        for (let i = id; i < id + 1; i++) {
+            const element = b[i];
+            element.style.width = "360px";
+            element.style.height = "360px";
+        }
+        lastid = -1;
+        lastobj = "obj";
+    }
+    else if (obj = "b") {
+        b = document.getElementsByClassName("bbox")
+        for (let i = 0; i < b.length; i++) {
+            const element = b[i];
+            if (i == id) {
+                element.style.width = "auto";
+                element.style.height = "auto";
+            }
+            else {
+                element.style.width = "360px";
+                element.style.height = "360px";
+            }
+        }
+        lastid = id;
+        lastobj = obj;
     }
 }
