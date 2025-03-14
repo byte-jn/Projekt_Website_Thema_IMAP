2f2b02d679d5576b68472f9608c64487443f6e27
Jannis Lauer <jannis280@outlook.de>
2025-03-14 20:34:27 +0100

    mehr anzeigen knopf

--- site/script.js
+++ site/script.js
@@ -11,3 +11,20 @@ function switchdarklightmode() {
         black = true;
     }
 }
+
+function gigainfo(num) {
+    let button = document.getElementById(`gib${num}`);
+    let ps = document.getElementsByClassName(`s${num}`);
+    let isVisible = button.dataset.visible === "true";
+
+    // Button-Text umschalten
+    button.innerHTML = isVisible ? "mehr anzeigen" : "weniger anzeigen";
+
+    // Sichtbarkeit der Elemente umschalten
+    for (let i = 0; i < ps.length; i++) {
+        ps[i].style.display = isVisible ? "none" : "block";  // Sichtbarkeit umschalten
+    }
+
+    // Zustand speichern
+    button.dataset.visible = isVisible ? "false" : "true";
+}
